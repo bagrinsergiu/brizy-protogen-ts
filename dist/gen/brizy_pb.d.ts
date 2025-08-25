878974607a2fcdd906ba5c66f864a309545c1770
alecszaharia <alecszaharia@users.noreply.github.com>
2025-08-25 10:53:52 +0000

    Tag: v0.0.31-a Message:

--- dist/gen/brizy_pb.d.ts
+++ dist/gen/brizy_pb.d.ts
@@ -8,6 +8,7 @@ export type CreateCollectionItemMessage = Message<"brizyMessage.brizy.CreateColl
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const CreateCollectionItemMessageSchema: GenMessage<CreateCollectionItemMessage>;
 export type UpdateCollectionItemMessage = Message<"brizyMessage.brizy.UpdateCollectionItemMessage"> & {
@@ -16,6 +17,7 @@ export type UpdateCollectionItemMessage = Message<"brizyMessage.brizy.UpdateColl
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const UpdateCollectionItemMessageSchema: GenMessage<UpdateCollectionItemMessage>;
 export type DeleteCollectionItemMessage = Message<"brizyMessage.brizy.DeleteCollectionItemMessage"> & {
@@ -24,6 +26,7 @@ export type DeleteCollectionItemMessage = Message<"brizyMessage.brizy.DeleteColl
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const DeleteCollectionItemMessageSchema: GenMessage<DeleteCollectionItemMessage>;
 export type CreateCollectionTypeMessage = Message<"brizyMessage.brizy.CreateCollectionTypeMessage"> & {
@@ -32,6 +35,7 @@ export type CreateCollectionTypeMessage = Message<"brizyMessage.brizy.CreateColl
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const CreateCollectionTypeMessageSchema: GenMessage<CreateCollectionTypeMessage>;
 export type UpdateCollectionTypeMessage = Message<"brizyMessage.brizy.UpdateCollectionTypeMessage"> & {
@@ -40,6 +44,7 @@ export type UpdateCollectionTypeMessage = Message<"brizyMessage.brizy.UpdateColl
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const UpdateCollectionTypeMessageSchema: GenMessage<UpdateCollectionTypeMessage>;
 export type DeleteCollectionTypeMessage = Message<"brizyMessage.brizy.DeleteCollectionTypeMessage"> & {
@@ -48,6 +53,7 @@ export type DeleteCollectionTypeMessage = Message<"brizyMessage.brizy.DeleteColl
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const DeleteCollectionTypeMessageSchema: GenMessage<DeleteCollectionTypeMessage>;
 export type CreateCustomerMessage = Message<"brizyMessage.brizy.CreateCustomerMessage"> & {
@@ -56,6 +62,7 @@ export type CreateCustomerMessage = Message<"brizyMessage.brizy.CreateCustomerMe
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const CreateCustomerMessageSchema: GenMessage<CreateCustomerMessage>;
 export type UpdateCustomerMessage = Message<"brizyMessage.brizy.UpdateCustomerMessage"> & {
@@ -64,6 +71,7 @@ export type UpdateCustomerMessage = Message<"brizyMessage.brizy.UpdateCustomerMe
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const UpdateCustomerMessageSchema: GenMessage<UpdateCustomerMessage>;
 export type DeleteCustomerMessage = Message<"brizyMessage.brizy.DeleteCustomerMessage"> & {
@@ -72,5 +80,33 @@ export type DeleteCustomerMessage = Message<"brizyMessage.brizy.DeleteCustomerMe
     entityIri: string;
     brziyApiAccessToken: string;
     userId: string;
+    cloudAccessToken: string;
 };
 export declare const DeleteCustomerMessageSchema: GenMessage<DeleteCustomerMessage>;
+export type CreateSymbolMessage = Message<"brizyMessage.brizy.CreateSymbolMessage"> & {
+    descriminator: Message_DescriminatorType;
+    projectIri: string;
+    entityIri: string;
+    brziyApiAccessToken: string;
+    userId: string;
+    cloudAccessToken: string;
+};
+export declare const CreateSymbolMessageSchema: GenMessage<CreateSymbolMessage>;
+export type UpdateSymbolMessage = Message<"brizyMessage.brizy.UpdateSymbolMessage"> & {
+    descriminator: Message_DescriminatorType;
+    projectIri: string;
+    entityIri: string;
+    brziyApiAccessToken: string;
+    userId: string;
+    cloudAccessToken: string;
+};
+export declare const UpdateSymbolMessageSchema: GenMessage<UpdateSymbolMessage>;
+export type DeleteSymbolMessage = Message<"brizyMessage.brizy.DeleteSymbolMessage"> & {
+    descriminator: Message_DescriminatorType;
+    projectIri: string;
+    entityIri: string;
+    brziyApiAccessToken: string;
+    userId: string;
+    cloudAccessToken: string;
+};
+export declare const DeleteSymbolMessageSchema: GenMessage<DeleteSymbolMessage>;
